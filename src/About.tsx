fff3ace74badf7f3071d527e832248f5646ddc1e
Mohammad Zeyad <mohammad.jzx100@gmail.com>
2026-07-16 11:40:57 +0300

    Update image

--- src/About.tsx
+++ src/About.tsx
@@ -45,7 +45,7 @@ export default function About() {
             <div className="w-full max-w-sm h-80 sm:h-96 rounded-[1.5rem] overflow-hidden bg-white/30">
               {!imgError ? (
                 <img
-  src="/5965194032499592670_121.png"
+ src="/Gemini_Generated_Image_l4gltvl4gltvl4gl_(1) copy.png"
   alt="Nawar working"
   className="w-full h-full object-cover grayscale"
   onError={() => setImgError(true)}
